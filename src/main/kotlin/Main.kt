5290f064ea63f19e335f25d75ac468f250e519ee
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-05-12 11:50:58 +0300

    ввод текста

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -14,13 +23,28 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
+    //var text = "Hello, World!"
 
     MaterialTheme {
+        Content(Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier){
+    var text by remember { mutableStateOf("") }
+    Column(modifier = modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier.size(8.dp))
         Button(onClick = {
-            text = "Hello, Desktop!"
         }) {
-            Text(text)
+            Text("Нажать нежно")
         }
+        Text(text, modifier = Modifier.fillMaxWidth())
     }
 }
 
